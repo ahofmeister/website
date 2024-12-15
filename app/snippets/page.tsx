593331ac3175770e11fb0c0319f677a820726281
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-12-15 08:25:10 +0100

    feat: Improve design for snippet list

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -2,6 +2,7 @@ import {compareDesc, format, parseISO} from 'date-fns'
 import {allSnippets, Snippet} from 'contentlayer/generated'
 import Link from 'next/link'
 import {Badge} from "@/components/ui/badge";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 
 async function getSnippets() {
     return allSnippets.sort((a, b) => {
@@ -16,24 +17,37 @@ export default async function Snippets() {
     return (
         <div className="mx-auto max-w-2xl py-16">
             {snippets.map((snippet, id) => (
-                <Snippet key={id} {...snippet} />
+                <>
+                    <Snippet key={id} {...snippet} />
+                </>
             ))}
         </div>)
 }
 
 
 const Snippet = (snippet: Snippet) => (
-    <div className="mb-3 py-6 border-b ">
-        <time dateTime={snippet.publishedAt} className="block text-sm mb-5">
-            {format(parseISO(snippet.publishedAt), 'LLLL d, yyyy')}
-        </time>
-        <h2 className="text-lg">
-            <Link href={snippet.slug}>
-                {snippet.title}
-            </Link>
-        </h2>
-        <div className={'mt-2 flex flex-row gap-2'}>{snippet.tags.map(tag => <Badge
-            key={tag}>{tag}</Badge>)}
-        </div>
-    </div>
+    <Card className="mb-3">
+        <Link href={snippet.slug}>
+            <CardHeader>
+                <CardTitle>
+                    <h2 className="text-lg">
+                        {snippet.title}
+                    </h2>
+                </CardTitle>
+                <CardDescription>
+                    <time dateTime={snippet.publishedAt} className="block text-sm mb-5">
+                        {format(parseISO(snippet.publishedAt), 'LLLL d, yyyy')}
+                    </time>
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <div className={'mt-2 flex flex-row gap-2'}>{snippet.tags.map(tag => <Badge
+                    key={tag}>{tag}</Badge>)}
+                </div>
+            </CardContent>
+
+
+        </Link>
+    </Card>
 )
