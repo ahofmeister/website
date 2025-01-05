16ceebe14cce1465887bf4a7d9fef37b54da765b
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-05 21:12:24 +0100

    feat: Change end date of last work

--- content/cv/about-data.ts
+++ content/cv/about-data.ts
@@ -27,7 +27,7 @@ export const aboutData = {
                 company: "Protofy GmbH & Co. KG",
                 startDate: "Jan 8, 2024",
                 endDate: "Oct 31, 2024",
-                description: "At Protofy, I am responsible for managing client projects, with focus on developing a web app that allow users to create, analyse, and train chess openings. My work covers both front-end and back-end development to ensure a smooth and efficient user experience.",
+                description: "At Protofy, I was responsible for managing client projects, with focus on developing a web app that allow users to create, analyse, and train chess openings. My work covered both front-end and back-end development to ensure a smooth and efficient user experience.",
                 technologies: ["Next.js", "React", "JavaScript", "Tailwind CSS", "shadcn-ui", "Supabase", "PostgreSQL", "Resend", "Docker", "Flutter"]
             },
             {
