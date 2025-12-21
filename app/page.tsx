2d71e4cffcc5d67a4343b69f9ed977f9a6174a8c
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-12-21 16:04:20 +0100

    Remove contentlayer, use biome, upgrade tailwind, Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
-    return (
-        <div>
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 
-        </div>
-    )
+export default function Home() {
+	return (
+		<>
+			<h1 className="text-xl md:text-2xl font-medium leading-13 mb-1">
+				Alexander Hofmeister
+			</h1>
+			<div className={"my-5"}>
+				I’m a developer with over 15 years of experience in software
+				development.
+			</div>
+			<div className={"my-5"}>
+				In recent years, I’ve focused on building modern web applications.
+			</div>
+			<div className={"my-5"}>
+				I'd like to make software easy, and fun to use.
+			</div>
+
+			<div className={"my-5"}>
+				<Link
+					href={"/projects"}
+					className={"hover:underline flex gap-x-1 items-center w-fit"}
+				>
+					Check out my projects here <ArrowRightIcon size={14} />
+				</Link>
+			</div>
+		</>
+	);
 }
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
-import {usePathname} from "next/navigation"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-import {cn} from "@/lib/utils"
-import {hr} from "date-fns/locale";
-
-const NavbarLink = ({href, label, currentPath}: {
-    href: string,
-    label: string,
-    currentPath: string
-}) =>
-    <Link href={href}
-          className={cn(
-              "transition-colors hover:text-primary",
-              currentPath === href ? "text-primary" : "text-foreground/60"
-          )}>
-        {label}
-    </Link>
-
+const NavbarLink = ({
+	href,
+	label,
+	currentPath,
+}: {
+	href: string;
+	label: string;
+	currentPath: string;
+}) => (
+	<Link
+		href={href}
+		className={cn(
+			"transition-colors hover:text-primary",
+			currentPath === href ? "text-primary" : "text-foreground/60",
+		)}
+	>
+		{label}
+	</Link>
+);
 
 export function MainNav() {
-    const pathname = usePathname()
+	const pathname = usePathname();
 
-    return (
-        <div className="hidden md:flex justify-center items-center w-full">
-            <nav className="flex text-center justify-center gap-4 text-sm lg:gap-6">
-                <NavbarLink href={'/'} label={'Home'} currentPath={pathname}/>
-                <NavbarLink href={'/about'} label={'About'} currentPath={pathname}/>
-                <NavbarLink href={'/snippets'} label={'Snippets'} currentPath={pathname}/>
-                <NavbarLink href={'/projects'} label={'Projects'} currentPath={pathname}/>
-            </nav>
-        </div>
-    );
+	return (
+		<div className="hidden md:flex justify-center items-center w-full">
+			<nav className="flex text-center justify-center gap-4 text-sm lg:gap-6">
+				<NavbarLink href={"/"} label={"Home"} currentPath={pathname} />
+				<NavbarLink
+					href={"/projects"}
+					label={"Projects"}
+					currentPath={pathname}
+				/>
+			</nav>
+		</div>
+	);
 }
 
-export default MainNav
+export default MainNav;
